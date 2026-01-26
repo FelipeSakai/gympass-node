00a89007da7cf8ce6b2264efa7da5a21cf15a115
FelipeSakai <Felipesakaiipolito@gmail.com>
2026-01-26 11:27:05 -0400

    feat: add Prisma setup with PostgreSQL integration

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
 import fastify from "fastify";
+import { prisma } from "../lib/prisma.js";
 
 export const app = fastify();
+
+
+prisma.user.create({
+    data: {
+        name: 'Felipe Sakai',
+        email: 'felipesakaiipolito@gmail.com'
+    }
+})
