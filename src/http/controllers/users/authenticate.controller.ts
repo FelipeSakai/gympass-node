84cb9f1c37046d965bb8c7768838fe41e19504d6
Felipe Sakai <felipesakaiipolito@gmail.com>
2026-03-17 00:03:30 -0400

    feat: add refresh token functionality with cookie support and update user routes

--- src/http/controllers/users/authenticate.controller.ts
+++ src/http/controllers/users/authenticate.controller.ts
@@ -3,30 +3,54 @@ import { makeAuthenticateService } from "@/services/factories/makeAuthenticateSe
 import type { FastifyReply, FastifyRequest } from "fastify";
 import z from "zod";
 
-export async function authenticateController(request: FastifyRequest, reply: FastifyReply) {
-    const authenticateBodySchema = z.object({
-        email: z.string().email(),
-        password: z.string().min(6),
-    })
+export async function authenticateController(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const authenticateBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
 
-    const { email, password } = authenticateBodySchema.parse(request.body);
+  const { email, password } = authenticateBodySchema.parse(request.body);
 
-    try {
-        const authenticateService = makeAuthenticateService();
+  try {
+    const authenticateService = makeAuthenticateService();
 
-        const { user } = await authenticateService.execute({ email, password });
-        const token = await reply.jwtSign({}, {
-            sign: {
-                sub: user.id,
-            },
-        });
+    const { user } = await authenticateService.execute({ email, password });
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      },
+    );
 
-        return reply.status(200).send({ token });
-    } catch (err) {
-        if (err instanceof InvalidCredentialsError) {
-            return reply.status(400).send({ message: err.message });
-        }
+    const refreshToken = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+          expiresIn: "7d",
+        },
+      },
+    );
 
-        throw err;
+    return reply
+      .setCookie("refreshToken", refreshToken, {
+        path: "/",
+        secure: true,
+        sameSite: true,
+        httpOnly: true,
+      })
+      .status(200)
+      .send({ token });
+  } catch (err) {
+    if (err instanceof InvalidCredentialsError) {
+      return reply.status(400).send({ message: err.message });
     }
+
+    throw err;
+  }
 }
